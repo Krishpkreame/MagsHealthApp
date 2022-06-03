cd480e038cbb136b9af3a4ac68f22ace625d37df
Krishpkreame <79666419+Krishpkreame@users.noreply.github.com>
2022-06-03 14:41:33 +1200

    clean up
    Add commets and got ready for app making using wireframes

--- main.py
+++ main.py
@@ -1,42 +1,37 @@
+# Base libaries
 import tkinter as tk
 from tkinter import ttk
+# Import pages
 import tkPages
-# temp packages
-import threading
-import time
 
 
 class App(ttk.Frame):
     def __init__(self, parent):
         ttk.Frame.__init__(self)  # initialize the superclass (frame)
-        # Vars -
-        self.started = False
-        self.count = 0
+        #
         # Page list
+        # ADD NEW CLASSES YOU MAKE TO LIST!  (pages will be indexed chronologically)
         self.availablePages = [
             tkPages.homepage,
-            tkPages.page1]
+            tkPages.testpage,
+            tkPages.workoffthis]
+        #
+        #
         # Split into 3 colums
-        for a in [0, 1, 2]:
-            self.columnconfigure(index=a, weight=1)
+        self.columnconfigure(index=0, weight=1)
+        self.columnconfigure(index=1, weight=1)
+        self.columnconfigure(index=2, weight=1)
         # Show first page on list at start up
-        self.availablePages[0].show(self)
+        self.availablePages[0].create(self)
 
+    # Function to change between pages
     def changePage(self, nmbr):
-        for widget in self.winfo_children():
-            widget.destroy()
-        self.availablePages[nmbr].show(self)
-
-    def timer(self):
-        time.sleep(3)
-        self.label.config(text=str(self.count))
-
-    def cpstest(self):
-        if(self.started == False):
-            self.label.config(text="CLICK!!!")
-            self.started = True
-            threading.Thread(target=self.timer).start()
-        self.count += 1
+        for widget in self.winfo_children():  # For each widget on scren
+            widget.destroy()  # Destory each widget found
+        #
+        #
+        # Run the create function on the desired page
+        self.availablePages[nmbr].create(self)
 
 
 if __name__ == "__main__":
--- tkPages.py
+++ tkPages.py
@@ -1,33 +1,68 @@
-from re import S
+# Import Base Libs
 import tkinter as tk
 from tkinter import ttk
 
+"""
+Make a class for every new page you want to create, this is to better organize your TK pages.
+In the class make a function called create which contains widgets(Labels, Images, TextFeilds)
+that you want to on that page.
+When making widgets ensure to always use 'self.' before the naming the var.
+Always use grid, not pack or place
+"""
+########################## - Program - ##########################
 
-class homepage():
-    def show(self):
+
+class homepage():  # Home page class
+    def create(self):  # Function will create widgets when invoked
+        # Get the photo from computer
         self.photo = tk.PhotoImage(file='./img/logo.png')
+        # Make a label using the image we got
         self.image_label = ttk.Label(
             self,
             image=self.photo,
-            padding=5
         )
-        self.image_label.grid(row=1, column=1, padx=15, pady=10)
+        # Place on grid into the window
+        self.image_label.grid(
+            row=1,
+            column=1,
+            pady=10)
+        # Create a button that will call the changePage call (in the main file)
         self.button = ttk.Button(
-            self, text="Next Page", command=lambda: self.changePage(1))
-        self.button.grid(row=2, column=1, padx=15, pady=10)
+            self,
+            text="Next Page",
+            command=lambda: self.changePage(1))  # ChangePage 1, meaning it will open index 1 of pages list
+        # Place on grid into the window
+        self.button.grid(
+            row=2,
+            column=1,
+            pady=10)
 
 
-class page1():
-    def show(self):
+# no comments u get the idea
+class testpage():
+    def create(self):
         self.label = ttk.Label(
             self,
-            text="Logo",
-            font=("-size", 15, "-weight", "bold"),
+            text="Testing",
         )
-        self.label.grid(row=1, column=1, pady=10)
+        self.label.grid(
+            row=1,
+            column=1,
+            pady=10)
         self.button = ttk.Button(
-            self, text="Last Page", command=lambda: self.changePage(0))
-        self.button.grid(row=2, column=1, pady=10)
-        self.button = ttk.Button(
-            self, text="SetText", command=lambda: self.cpstest())
-        self.button.grid(row=3, column=1, padx=15, pady=10)
+            self,
+            text="Prev Page",
+            command=lambda: self.changePage(0))  # Note that I used 0 because the homepage is the 0th index in list
+        self.button.grid(
+            row=2,
+            column=1,
+            pady=10)
+
+
+class workoffthis():
+    def create(self):
+        self.label = ttk.Label(text="Krish Patel")
+        self.label.grid(row=1, column=1)
+        #
+        # CODE HERE
+        #
