d2440c59030edc3d13ec5ac8d3513fadfe67e8cc
Krishpkreame <79666419+Krishpkreame@users.noreply.github.com>
2022-06-10 14:19:35 +1200

    hashed pswd 2, with proper pages

--- main.py
+++ main.py
@@ -9,13 +9,19 @@ import tkPages
 
 class App(ttk.Frame):
     def __init__(self, parent):
+        # Setup some styling
+        s = ttk.Style()
+        s.configure('small.TButton', font=(None, 7))
+        s.configure('big.TButton', font=(None, 18))
+
         ttk.Frame.__init__(self)  # initialize the superclass (frame)
         #
         # Page list
         # ADD NEW CLASSES YOU MAKE TO LIST!  (pages will be indexed chronologically)
         self.availablePages = [
             tkPages.loginpage,
-            tkPages.testpage]
+            tkPages.signpage,
+            tkPages.mainpage]
         #
         #
         # Split into 3 colums
@@ -36,17 +42,41 @@ class App(ttk.Frame):
 
     # Login func to login and move to next screen
     def login(self, email, password):  # ! Add comments
+        # DB connection
         conn = pymysql.connect(host='sql6.freemysqlhosting.net', port=3306,
                                user='sql6498570', passwd='AJ6NHabilg', db='sql6498570')
         cur = conn.cursor()
         cur.execute(
             "SELECT `pswdHash` FROM MagsHealthApp WHERE `email`=%s", (email))
         result = cur.fetchone()
+        #
+        # Password checking
         if bcrypt.checkpw(bytes(password, 'utf-8'), bytes(result[0], 'utf-8')):
             print("Correct password")
             self.changePage(1)
         else:
             print("Incorrect password")
+
+    # Signup func to create new user in db
+    def signup(self, name, email, password, confirmPassword):  # ! Add comments
+        # DB connection
+        conn = pymysql.connect(host='sql6.freemysqlhosting.net', port=3306,
+                               user='sql6498570', passwd='AJ6NHabilg', db='sql6498570')
+        cur = conn.cursor()
+        #
+        # Check if values are valid
+        if password == confirmPassword:
+            print("Password matchs")
+            # Hash password
+            hashed = bcrypt.hashpw(
+                bytes(password.strip(), 'utf-8'), bcrypt.gensalt())
+            print(hashed)
+            # Insert into DB
+            sql = """insert into MagsHealthApp (name, email, pswdHash, weight)
+                    values (%s, %s, %s, %s)
+            """
+            cur.execute(sql, (name, email, hashed, 0))
+            conn.commit()
 
 
 if __name__ == "__main__":  # If this file is run directly, run the following code
--- tkPages.py
+++ tkPages.py
@@ -4,6 +4,7 @@ from tkinter import ttk
 
 # Temp libs
 import random
+from turtle import bgcolor
 
 
 """
@@ -16,7 +17,56 @@ Always use grid, not pack or place
 ########################## - Program - ##########################
 
 
-class loginpage():  # Home page class
+class loginpage():  # login page class
+    def create(self):  # Function will create widgets when invoked
+        # Get the photo from computer
+        self.photo = tk.PhotoImage(file='./img/logo.png')
+
+        # Make a label using the image we got
+        self.image_label = ttk.Label(self, image=self.photo,)
+        self.image_label.grid(row=1, column=1)  # Place 1st row
+
+        # Create a Frame for login widgets
+        self.loginentrys = ttk.Frame(self)  # Frame
+        self.loginentrys.grid(row=2, column=1, sticky="nsew", rowspan=2)
+        # Setup columns
+        self.loginentrys.columnconfigure(index=0, weight=1)
+        self.loginentrys.columnconfigure(index=1, weight=1)
+
+        # Email Entry ---
+        self.emalimg = tk.PhotoImage(file='./img/email.png')  # Get email icon
+        # Create icon in nested 1st column and email entry in 2nd
+        self.emalicon = ttk.Label(self.loginentrys, image=self.emalimg)
+        self.emalicon.grid(row=0, column=0, sticky="ne", pady=10)
+        self.email = ttk.Entry(self.loginentrys)
+        self.email.grid(row=0, column=1, pady=10)
+
+        # Password Entry ---
+        self.pswdimg = tk.PhotoImage(file='./img/pswd.png')  # Get pswd icon
+        # Create icon in nested 1st column and password entry in 2nd but 2nd row
+        self.pswdicon = ttk.Label(self.loginentrys, image=self.pswdimg)
+        self.pswdicon.grid(row=1, column=0, sticky="ne", pady=10)
+        self.pswd = ttk.Entry(self.loginentrys)
+        self.pswd.grid(row=1, column=1, pady=10)
+
+        # Create a button that will call the changePage call (in the main file)
+        self.loginBtn = ttk.Button(
+            self,
+            text="Login",
+            style="big.TButton",
+            command=lambda: self.login(self.email.get(), self.pswd.get()))  # ChangePage 1, meaning it will open index 1 of pages list
+        self.loginBtn.grid(row=4, column=1, pady=10)  # Place 4th row
+
+        # Signup button
+        self.signupBtn = ttk.Button(
+            self,
+            text="Signup",
+            style="small.TButton",
+            command=lambda: self.changePage(1))  # ChangePage 1, meaning it will open index 1 of pages list
+        self.signupBtn.grid(row=5, column=1, pady=10)  # Place 4th row
+
+
+class signpage():  # signup page page class
     def create(self):  # Function will create widgets when invoked
         # Get the photo from computer
         self.photo = tk.PhotoImage(file='./img/logo.png')
@@ -32,38 +82,50 @@ class loginpage():  # Home page class
         self.loginentrys.columnconfigure(index=0, weight=1)
         self.loginentrys.columnconfigure(index=1, weight=1)
 
-        # Username Entry ---
-        self.userimg = tk.PhotoImage(file='./img/user.png')  # Get user icon
-        # Create icon in nested 1st column and username entry in 2nd
-        self.usericon = ttk.Label(self.loginentrys, image=self.userimg)
-        self.usericon.grid(row=0, column=0, sticky="ne", pady=10)
+        # Name Entry ---
+        self.nameimg = tk.PhotoImage(file='./img/user.png')  # Get name icon
+        # Create icon in nested 1st column and name entry in 2nd
+        self.nameicon = ttk.Label(self.loginentrys, image=self.nameimg)
+        self.nameicon.grid(row=0, column=0, sticky="ne", pady=10)
+        self.name = ttk.Entry(self.loginentrys)
+        self.name.grid(row=0, column=1, pady=10)
+
+        # Email Entry ---
+        self.emalimg = tk.PhotoImage(file='./img/email.png')  # Get email icon
+        # Create icon in nested 1st column and email entry in 2nd
+        self.emalicon = ttk.Label(self.loginentrys, image=self.emalimg)
+        self.emalicon.grid(row=1, column=0, sticky="ne", pady=10)
         self.email = ttk.Entry(self.loginentrys)
-        self.email.grid(row=0, column=1, pady=10)
+        self.email.grid(row=1, column=1, pady=10)
 
         # Password Entry ---
         self.pswdimg = tk.PhotoImage(file='./img/pswd.png')  # Get pswd icon
         # Create icon in nested 1st column and password entry in 2nd but 2nd row
         self.pswdicon = ttk.Label(self.loginentrys, image=self.pswdimg)
-        self.pswdicon.grid(row=1, column=0, sticky="ne", pady=10)
+        self.pswdicon.grid(row=2, column=0, sticky="ne", pady=10)
         self.pswd = ttk.Entry(self.loginentrys)
-        self.pswd.grid(row=1, column=1, pady=10)
+        self.pswd.grid(row=2, column=1, pady=10)
+
+        # Confirm Password Entry ---
+        self.conPswdicon = ttk.Label(self.loginentrys, image=self.pswdimg)
+        self.conPswdicon.grid(row=3, column=0, sticky="ne", pady=10)
+        self.conPswd = ttk.Entry(self.loginentrys)
+        self.conPswd.grid(row=3, column=1, pady=10)
 
         # Create a button that will call the changePage call (in the main file)
         self.button = ttk.Button(
             self,
-            text="Login",
-            command=lambda: self.login(self.email.get(), self.pswd.get()))  # ChangePage 1, meaning it will open index 1 of pages list
+            text="Signup",
+            command=lambda: self.signup(self.name.get(), self.email.get(), self.pswd.get(), self.conPswd.get()))  # ChangePage 1, meaning it will open index 1 of pages list
         self.button.grid(row=4, column=1, pady=10)  # Place 4th row
 
 
 # no comments u get the idea
-class testpage():
+class mainpage():
     def create(self):
-        lines = open('quotes.txt').read().splitlines()
-        self.randQuote = random.choice(lines)
         self.label = ttk.Label(
             self,
-            text=self.randQuote,
+            text="Login successful!\nWelcome to the main page!",
             font=("Arial", 25)
         )
         self.label.grid(
@@ -72,7 +134,7 @@ class testpage():
             pady=10)
         self.button = ttk.Button(
             self,
-            text="Prev Page",
+            text="Logout",
             command=lambda: self.changePage(0))  # Note that I used 0 because the homepage is the 0th index in list
         self.button.grid(
             row=2,
